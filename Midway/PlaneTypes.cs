d35efe196f668eea260ef2753bf6f704d51367e0
WilcoxV <vwilcox2000@hotmail.com>
2021-06-18 21:16:17 -0500

    Implemented much more of game

---
+++ Midway/PlaneTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidwayEngine
+{
+  public enum PlaneTypes
+  {
+    Fighter,
+    Bomber,
+    TorpedoBomber
+  }
+}
